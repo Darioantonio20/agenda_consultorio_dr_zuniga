fa595d470a754540959505b5df7669db4b6c6514
Darioantonio20 <antoniodario10@hotmail.com>
2025-06-10 18:34:53 -0600

    FIX: idioma de la aplicación

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart';
 import 'package:agenda_dr_zuniga/providers/appointment_provider.dart';
 import 'package:agenda_dr_zuniga/providers/patient_provider.dart';
 import 'package:agenda_dr_zuniga/views/home_view.dart';
@@ -7,6 +9,7 @@ import 'package:agenda_dr_zuniga/utils/database_helper.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Intl.defaultLocale = 'es_ES';
   await DatabaseHelper().init(); // Initialize the database
   runApp(const MyApp());
 }
@@ -26,6 +29,14 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.deepPurple,
         ),
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('es', 'ES'),
+        ],
         home: const HomeView(),
         debugShowCheckedModeBanner: false,
       ),
